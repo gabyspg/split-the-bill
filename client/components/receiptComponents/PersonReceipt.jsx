2c2172b30d1d60ee09f15fe680385240d37de6a5
gabyspg <gabspg@gmail.com>
2024-11-23 12:29:50 -0500

    added conditional rendering for receipts when a person has a total of /bin/zsh

--- client/components/receiptComponents/PersonReceipt.jsx
+++ client/components/receiptComponents/PersonReceipt.jsx
@@ -25,23 +25,29 @@ const PersonReceipt = (props) => {
       <div className="receipt-header">
         <h3>Receipt: {person}</h3>
       </div>
-      <div className="receipt-items">
-        <div className="item-row">
-          <span className="item-quantity">#</span>
-          <span className="item-name">Items</span>
-          <span className="item-price">Price</span>
-        </div>
-        <hr className="line"></hr>
-        {itemsList}
-      </div>
-      <hr className="line"></hr>
-      <div className="receipt-summary">
-        <SummaryRow name={'Subtotal'} price={subtotal} />
-        <SummaryRow name={'Tax'} price={tax} />
-        <SummaryRow name={'Tip'} price={tip} />
-        <hr className="line"></hr>
-        <SummaryRow name={'Total'} price={total} />
-      </div>
+      {total ? (
+        <>
+          <div className="receipt-items">
+            <div className="item-row">
+              <span className="item-quantity">#</span>
+              <span className="item-name">Items</span>
+              <span className="item-price">Price</span>
+            </div>
+            <hr className="line"></hr>
+            {itemsList}
+          </div>
+          <hr className="line"></hr>
+          <div className="receipt-summary">
+            <SummaryRow name={'Subtotal'} price={subtotal} />
+            <SummaryRow name={'Tax'} price={tax} />
+            <SummaryRow name={'Tip'} price={tip} />
+            <hr className="line"></hr>
+            <SummaryRow name={'Total'} price={total} />
+          </div>
+        </>
+      ) : (
+        'Lucky you! Nothing to pay this time.'
+      )}
     </div>
   );
 };
